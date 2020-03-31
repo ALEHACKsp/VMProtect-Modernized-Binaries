b5103695a33348f1baafec871ebfe11e4211ff8d
thatfox <39231907+thatfox@users.noreply.github.com>
2020-03-30 18:48:14 -0700

    c_api_date.hh
    This is the main struct for the annual checks upon software.

---
+++ c_api_date.hh
@@ -0,0 +1,17 @@
+
+/*
+ * VMProtect Security Instructional Demo | Written by https://github.com/thatfox
+ */
+
+#pragma once
+// Name: VMProtect Security Date [...]
+// Code...	
+struct c_date_t
+{
+
+	long year {};
+	unsigned char month {};
+	unsigned char day {};
+
+};
+// End...
