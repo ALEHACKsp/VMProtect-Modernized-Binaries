dcc8323b3c7d54098eaa9aff44a56be9c73783ae
thatfox <39231907+thatfox@users.noreply.github.com>
2020-03-30 18:46:59 -0700

    c_api.hh
    This essentially is the include header. All your primary functions will be added here so that they can be used within the interface reference(s).

---
+++ c_api.hh
@@ -0,0 +1,28 @@
+
+/*
+ * VMProtect Security Instructional Demo | Written by https://github.com/thatfox
+ */
+
+#pragma once
+// Set: VMProtect 32Bit Library
+#pragma comment( lib, "VMProtectSDK32.lib" ) // Download: VMProtectSDK32.lib from the official website. 
+// Name: VMProtect Security API [...]
+// Code...
+
+// Include: Windows Math
+#include <Math.h>
+// Include: Windows Standard Int
+#include <stdint.h>
+// Include: Windows CoreMath
+#include <corecrt_math.h>
+// Include: VMProtect Flag
+#include "c_api_flag.hh"
+// Include: VMProtect Date
+#include "c_api_date.hh"
+// Include: VMProtect User State
+#include "c_api_user_state.hh"
+// Include: VMProtect Serial Data
+#include "c_api_serial_data.hh"
+// Include: VMProtect Function
+#include "c_api_function.hh"
+// End...
